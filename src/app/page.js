5046481a96778a69c138c8b492eb7f6362c65f73
Gavin-Persad <gavinapersad@gmail.com>
2024-04-12 13:33:36 +0100

    telling lies

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Gavin Persad</h1>
+      <h1>Gavin Persad is the best</h1>
     </div>
   );
 }

b801114c1c85f14b88535f60bb110e19f92b3fc7
Gavin-Persad <gavinapersad@gmail.com>
2024-04-15 18:05:30 +0100

    html for header added, need to style

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,5 +2,28 @@ import React from 'react';
 import styles from './header.module.css';
 
 export default function Header() {
-	return <header className={styles.head}>Header</header>;
+	return <header className={styles.head}>
+		<div className="logoContainer">
+			<img src=".../public\IMGs\_5315809a-b981-4f34-8ec7-a581b8980dfa.jpg" alt="Gavin Persad" />
+		</div>
+		<div className="linksContainer">
+			<ul>
+				<li>
+					<a href="/">About Me</a>
+				</li>
+				<li>
+					<a href="/about">Languages</a>
+				</li>
+				<li>
+					<a href="/contact">Projects</a>
+				</li>
+				<li>
+					<a href="/contact">Contact</a>
+				</li>
+				<li>
+					<button>Dark/Light</button>
+				</li>
+			</ul>
+		</div>
+	</header>;
 }

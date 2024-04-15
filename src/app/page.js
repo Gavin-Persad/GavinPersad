d626fa655f2f7fefa1119bd0e2bee43333273d99
Gavin-Persad <gavinapersad@gmail.com>
2024-04-15 16:17:10 +0100

    header and footer files created and linked

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Gavin Persad is the best!!!</h1>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h1>Gavin Persad is the best!!!</h1>
+		</div>
+	);
 }

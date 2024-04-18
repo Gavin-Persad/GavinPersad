b427329c9a2dbff9fb94c13c0cba66dd979cd37b
Gavin-Persad <gavinapersad@gmail.com>
2024-04-18 13:45:27 +0100

    testing image

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from './header.module.css';
 
 export default function Header() {
 	return <header className={styles.head}>
 		<div className="logoContainer">
-			<img src="/IMGs/_5315809a-b981-4f34-8ec7-a581b8980dfa.jpg" alt="Gavin Persad" />
+			<Image src="/IMGs/_5315809a-b981-4f34-8ec7-a581b8980dfa.jpg" alt="Gavin Persad" width={200} height={200} />
 		</div>
 		<div className="linksContainer">
 			<ul>
